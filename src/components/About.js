6ab601954d41277c46baa86ab423f653404abd4a
savini419 <stevensavini@gmail.com>
2017-06-04 14:55:02 -0400

    some styling

--- src/components/About.js
+++ src/components/About.js
@@ -1,23 +1,27 @@
 import React from 'react'
 
 const About = (props) => (
-  <div className="row mbl">
-    <div className="small-12 columns abt"><h3>{props.data.name}</h3></div>
-
-    <div class="small-12 columns imgContainer abt">
-    <div className="small-6 columns"><img src={props.data.image} alt={props.data.name} /></div>
-    <div className="small-6 columns"><img src={props.data.otherImage}/></div>
+  <div className="row">
+    <div className="small-12 columns">
+      <h3 className="aboutText">{props.data.name}</h3>
+      <h5 className="aboutText">{props.data.description}</h5>
+      <hr></hr>
     </div>
-    <div className="small-12 columns abt">{props.data.description}</div>
+
+    <div className="small-12 columns imgContainer aboutText">
+      <div className="small-12 columns"><a href={props.data.imageLink}><img src={props.data.image} alt={props.data.name} /></a></div>
+      <hr></hr>
+      <div className="small-12 columns"><a href={props.data.otherImageLink}><img src={props.data.otherImage}/></a></div>
+    </div>
   </div>
 )
 
 const Abouts = (props) => {
   let abouts = props.data.map((about, i) => {
       return <About key={`about-${i}`} data={about} />
-    })
-    return <div>{abouts}</div>
-  }
+  })
+  return <div>{abouts}</div>
+}
 
 
 
