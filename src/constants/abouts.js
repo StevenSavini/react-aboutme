20d824a3d21bd30cd0b8affd2298846bf3a1532c
savini419 <stevensavini@gmail.com>
2017-07-02 23:11:27 -0400

    more tweaks

--- src/constants/abouts.js
+++ src/constants/abouts.js
@@ -8,7 +8,7 @@ const abouts = [
 
   {
     menu_id: 'hire',
-    name: 'Why Hire Me',
+    name: 'Inpiration',
 <<<<<<< HEAD:src/constants/about.js
     description: 'Being a father of two children has given me perspective on life. It has made me assess what I want in life. I came into programming because I have a passion for it. I spent years prior to Launch practicing on sites like Code Academy and Free Code Camp in my free time after I put the kids to sleep. Knowing that I had found a career that would amke me happy I put everything on the line, quit my job, and paid to attend Launch Academy. Now I\'m ready to jump into a new career. Having an uderstanding of Ruby, React, JavaScript, OOP, TDD, and the Git workflow has put me into a position to succeed and I cannot wait to further my learning.',
     image: 'https://s3.amazonaws.com/horizon-production/images/restaurants/corner-pub.jpg'
@@ -20,7 +20,7 @@ const abouts = [
 
   {
     menu_id: 'project',
-    name: 'My Projects',
+    name: 'Projects',
     description: 'I made two showcase projects during my time at Launch Academy. Stream Director and TV A La Carte',
     image: 'https://s3.us-east-2.amazonaws.com/react-aboutme-development/Screen+Shot+2017-06-02+at+12.28.34+PM.png',
     imageLink: 'https://streamdirector.herokuapp.com/',
@@ -30,7 +30,7 @@ const abouts = [
 
   {
     menu_id: 'contact',
-    name: 'Contact',
+    name: 'Contact Me',
     description: 'Find me on Github & LinkedIn',
     image: 'https://s3.us-east-2.amazonaws.com/react-aboutme-development/github.png',
     imageLink: 'https://github.com/StevenSavini/',
